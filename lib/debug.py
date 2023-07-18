a013a6453bd8427617a70f4ac9112d1f99c0e614
Adam La Rosa <adam.larosa@flatironschool.com>
2023-07-18 12:21:44 -0700

    fin

--- lib/debug.py
+++ lib/debug.py
@@ -1,4 +1,8 @@
 import ipdb
-from models import session, Doctor
+from models import session, Doctor, Appointment, Patient
+
+d1 = session.query( Doctor ).first()
+a1 = session.query( Appointment ).first()
+p1 = session.query( Patient ).first()
 
 ipdb.set_trace()
--- lib/models.py
+++ lib/models.py
@@ -1,9 +1,11 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import ForeignKey, MetaData, create_engine
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.associationproxy import association_proxy
+
 convention = {
-    'fk': 'fk_%(table_name)s_%(column_name)s_%(referred_table_name)s'
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s'
 }
 md = MetaData( naming_convention = convention )
 
@@ -17,6 +19,44 @@ session = Session()
 
 class Doctor( Base ):
     __tablename__ = 'doctors'
-
     id = Column( Integer(), primary_key = True )
     name = Column( String() )
+
+    appointments = relationship( 'Appointment', back_populates = 'doctor' )
+    
+    patients = association_proxy( 'appointments', 'patient' )
+
+    def __repr__( self ):
+        return f'<Doctor id: {self.id} name: {self.name}>'
+
+# a doctor 'has many' apppointments
+# an appointment 'belongs to' a doctor
+# the 'belongs-to' side ALWAYS gets the foreign key.  ;) 
+
+class Appointment( Base ):
+    __tablename__ = 'appointments'
+    id = Column( Integer(), primary_key = True )
+    notes = Column( String() )
+    doctor_id = Column( Integer(), ForeignKey( 'doctors.id' ) )
+    patient_id = Column( Integer(), ForeignKey( 'patients.id' ) )
+    
+    doctor = relationship( 'Doctor', back_populates = 'appointments' )
+    patient = relationship( 'Patient', back_populates = 'appointments' )
+    
+    def __repr__( self ):
+        return f'<Appointment id: {self.id} notes: {self.notes}>'
+
+
+# a patient 'has many' appointments
+
+class Patient( Base ):
+    __tablename__ = 'patients'
+    id = Column( Integer(), primary_key = True )
+    name = Column( String() )
+
+    appointments = relationship( 'Appointment', back_populates = 'patient' )
+
+    doctors = association_proxy( 'appointments', 'doctor' )
+
+    def __repr__( self ):
+        return f'<Patient id: {self.id} name: {self.name}>'
