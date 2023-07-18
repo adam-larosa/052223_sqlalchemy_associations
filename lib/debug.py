9a32283b857b6fed1c5d83675a4ce0c85378715b
Adam La Rosa <adam.larosa@flatironschool.com>
2023-07-18 11:40:08 -0700

    Doctor model

---
+++ lib/debug.py
@@ -0,0 +1,4 @@
+import ipdb
+from models import session, Doctor
+
+ipdb.set_trace()
--- lib/models.py
+++ lib/models.py
@@ -1,10 +1,22 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import ForeignKey, MetaData
-
+from sqlalchemy import ForeignKey, MetaData, create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 convention = {
     'fk': 'fk_%(table_name)s_%(column_name)s_%(referred_table_name)s'
 }
 md = MetaData( naming_convention = convention )
 
-
 Base = declarative_base( metadata = md )
+
+engine = create_engine( 'sqlite:///funstuff.db' )
+Session = sessionmaker( bind = engine )
+
+session = Session()
+
+
+class Doctor( Base ):
+    __tablename__ = 'doctors'
+
+    id = Column( Integer(), primary_key = True )
+    name = Column( String() )
